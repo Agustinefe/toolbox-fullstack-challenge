14b63f2969419bb4bca4ad98534b15a36e3f82d1
Agustinefe <agustin.firmapaz@creativecoefficient.net>
2025-12-31 17:50:27 -0300

    Create react project

---
+++ src/front/src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "react-bootstrap/Button";
+
+export default function App() {
+  return (
+    <div>
+      <h1>React + Webpack + Babel 🚀</h1>
+      <p>Configuración manual</p>
+      <Button variant="primary">Click me</Button>
+    </div>
+  );
+}
---
+++ src/front/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true
+  },
+  mode: "development"
+};
