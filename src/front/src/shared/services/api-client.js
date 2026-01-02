a83613d27c21bd56b29c01ada9ff0faa26d1ee26
Agustinefe <agustin.firmapaz@creativecoefficient.net>
2026-01-01 23:30:34 -0300

    Add package to manage env vars

--- src/front/src/shared/services/api-client.js
+++ src/front/src/shared/services/api-client.js
@@ -1,5 +1,4 @@
-const BASE_URL = 'http://localhost:3001'
-
+const BASE_URL = process.env.REACT_APP_API_URL
 
 async function request(method, endpoint, searchParams = {}, options = {}) {
   const config = {
